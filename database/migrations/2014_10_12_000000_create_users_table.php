3bd19da04dd4b4268f66ed2467fac7a3ef27b44e
Ailten <faouzi26b@live.fr>
2025-12-19 18:45:58 +0100

    minor edit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,10 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->rememberToken();  // use to stay connected.
+            $table->integer('gold')->default(0);
+            $table->integer('gemme')->default(0);
+            //$table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
