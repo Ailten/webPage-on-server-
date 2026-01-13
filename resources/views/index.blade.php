d1b10e33da443c48cd4e30a09b5fced50f891766
Ailten <faouzi26b@live.fr>
2026-01-13 14:35:59 +0100

    index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,31 @@
 
 @section('contend')
 
-<p>line placeholder 1</p>
+<h1 class="text-center title-index">
+    Bienvenue sur {{ env('WEBSITE_NAME') }} !
+</h1>
+
+<p>
+    connectez vous avec twitch
+    @guest
+    (avec le bouton en haut a droite)
+    @endguest
+    @auth
+    (vous l'etes déja)
+    @endauth
+    .
+</p>
+
+<p>
+    créez votre premier personnage dans le menu "personnages".
+</p>
+
+<p>
+    rejoignez des combat dans le chat de streamer, gagnez des niveaux et des équipement rare.
+</p>
+
+<p>
+    amusez vous bien :)
+</p>
 
 @endsection
