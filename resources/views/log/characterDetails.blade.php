9279c5ee290af0a2448965123d3dcfc5efe1cdfb
Ailten <faouzi26b@live.fr>
2026-01-22 19:41:15 +0100

    edit page charater detials (grid position)

--- resources/views/log/characterDetails.blade.php
+++ resources/views/log/characterDetails.blade.php
@@ -20,25 +20,30 @@ $percentXp = ($character->xp / $xpNeedForLvlUp) *100;
 
 <div class="d-flex mt-4">
     <div>
-        <div class="character-details-illu-container">
-            <img src="{{ asset("img/class/{$character->characterSpecie->id}.png") }}">
-        </div>
-        <!-- can add a div heer-->
-    </div>
-    <div class="item-equiped-container">
-        @foreach ($inventoriesEquiped as $inventory)
-
-            <div class="item-container">
-
-                @if(!is_string($inventory))
-                    <img src="{{ asset("img/item/{$inventory->itemRef->item_category_id}.png") }}">
-                @else
-                    <p>{{  $inventory  }}</p>
-                @endif
-
+        <div class="char-item-container">
+            <div class="character-details-illu-container">
+                <!--<img src="{{ asset("img/class/{$character->characterSpecie->id}.png") }}">-->
+                <div class="div-img-illu-character" style="background-image: url({{ asset("img/class/{$character->characterSpecie->id}.png") }})"></div>
             </div>
-        
-        @endforeach
+            <div class="item-equiped-container">
+                @foreach ($inventoriesEquiped as $inventory)
+
+                    <div class="item-container">
+
+                        @if(!is_string($inventory))
+                            <img src="{{ asset("img/item/{$inventory->itemRef->item_category_id}.png") }}">
+                        @else
+                            <p><!--{{  $inventory  }}--></p>
+                        @endif
+
+                    </div>
+
+                @endforeach
+            </div>
+            <div style="height: 200px; margin-right: 15px; margin-top: 10px;">
+                <!-- other block -->
+            </div>
+        </div>
     </div>
     <div class="stat-container flex-grow-1">
         
