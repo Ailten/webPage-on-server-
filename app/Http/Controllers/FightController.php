88f724eba4f3f8ba300678dafe6adbebd1a91556
Ailten <faouzi26b@live.fr>
2026-02-03 14:27:59 +0100

    comment validator throwing error.

--- app/Http/Controllers/FightController.php
+++ app/Http/Controllers/FightController.php
@@ -14,17 +14,18 @@ class FightController extends Controller
 
     public function sendFormTwitchOption(Request $request) {
 
-        $pparamValidated = $request->validate([
-            'cmdJoin' => ['string', 'regex:/^![a-zA-Z_-]+ \{pseudo\}$/']
-        ], [
-            'cmdJoin.string' => 'cmdJoin doit être une commande (text).',
-            'cmdJoin.regex' => 'cmdJoin doit commencer par "!" et finir par "{pseudo}".'
-        ]);
+        // validator : return an object with param (crash if find an error).
+        //$paramValidated = $request->validate([
+        //    'cmdJoin' => ['string', 'regex:/^![a-zA-Z_-]+ \{pseudo\}$/']
+        //], [
+        //    'cmdJoin.string' => 'cmdJoin doit être une commande (text).',
+        //    'cmdJoin.regex' => 'cmdJoin doit commencer par "!" et finir par "{pseudo}".'
+        //]);
 
         return response()->json([
             'message' => 'it\'s work',
 
-            'params' => $pparamValidated
+            'params' => $paramValidated
         ]);
 
     }
