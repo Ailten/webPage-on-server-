5ef631419159752834344d456d80c28715a46c6f
Ailten <faouzi26b@live.fr>
2026-02-18 13:59:33 +0100

    debug inventory user

--- routes/web.php
+++ routes/web.php
@@ -166,6 +166,29 @@ Route::prefix('/debug')
     Route::get('/item-{id}', function (Request $request, $id) {
         return ItemRef::find($id);
     })->where(['id' => '[0-9]+'])
-    ->name('items');
+    ->name('item');
+    // debug inventory (by idUser).
+    Route::get('/inventory-{idUser}', function (Request $request, $idUser) {
+        return User::find($idUser)
+            ->inventories()
+            ->leftJoin('equipeds', 'equipeds.inventory_id', '=', 'inventories.id')
+            ->leftJoin('characters', 'characters.id', '=', 'equipeds.character_id')
+            ->join('item_refs', 'item_refs.id', '=', 'inventories.item_ref_id')
+            ->join('item_categories', 'item_categories.id', '=', 'item_refs.item_categorie_id')
+            ->join('item_rarities', 'item_rarities.id', '=', 'item_refs.item_rarity_id')
+            
+            ->paginate(10, [  // pagination.
+                'inventories.id as id',
+                'inventories.quantity as quantity',
+                DB::raw('COUNT(characters.id) as equiped_quantity'),
+                'item_refs.name as name',
+                'item_refs.price as price',
+                'item_refs.level as level',
+                'item_categories.name as item_categorie',
+                'item_rarities.name as item_rarity',
+
+            ]);
+    })->where(['idUser' => '[0-9]+'])
+    ->name('inventory');
 
 });
