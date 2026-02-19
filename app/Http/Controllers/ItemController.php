e509698d4b1bbd22a4fc57da3a4c1d731f5d46df
Ailten <faouzi26b@live.fr>
2026-02-19 12:34:26 +0100

    use query inventory in controller item

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -13,23 +13,7 @@ class ItemController extends Controller
         // get items from inventory user log (in a paginator).
         $items = (
             Auth::user()->inventories()  // get inventory of user log.
-            ->leftJoin('equipeds', 'equipeds.inventory_id', '=', 'inventories.id')
-            ->leftJoin('characters', 'characters.id', '=', 'equipeds.character_id')
-            ->join('item_refs', 'item_refs.id', '=', 'inventories.item_ref_id')
-            ->join('item_categories', 'item_categories.id', '=', 'item_refs.item_categorie_id')
-            ->join('item_rarities', 'item_rarities.id', '=', 'item_refs.item_rarity_id')
-            
-            ->paginate(10, [  // pagination.
-                'inventories.id as id',
-                'inventories.quantity as quantity',
-                DB::raw('COUNT(characters.id) as equiped_quantity'),
-                'item_refs.name as name',
-                'item_refs.price as price',
-                'item_refs.level as level',
-                'item_categories.name as item_categorie',
-                'item_rarities.name as item_rarity',
-
-            ])
+            ->withDetailsInventory()
         );
 
         // send characters to the view.
