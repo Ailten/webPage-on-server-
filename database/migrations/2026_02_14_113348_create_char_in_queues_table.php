923c613213f57eed689c41c817238113e9e1febc
Ailten <faouzi26b@live.fr>
2026-03-05 12:12:57 +0100

    edit down to migrate table

--- database/migrations/2026_02_14_113348_create_char_in_queues_table.php
+++ database/migrations/2026_02_14_113348_create_char_in_queues_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\BotTwitch;
+use App\Models\CharInQueue;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -28,6 +30,12 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::table('char_in_queues', function(Blueprint $table){
+            $table->dropForeignIdFor(BotTwitch::class);
+        });
+        Schema::table('char_in_queues', function(Blueprint $table){
+            $table->dropForeignIdFor(CharInQueue::class);
+        });
         Schema::dropIfExists('char_in_queues');
     }
 };
